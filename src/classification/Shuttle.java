a260454aa01a0610ea61b5888bafc95701e66914
Daniel Zárate <d.ansizgz@gmail.com>
2022-07-07 22:31:14 -0500

    Corrigiendo errores

--- src/classification/Shuttle.java
+++ src/classification/Shuttle.java
@@ -1,5 +1,28 @@
 package classification;
 
+import lombok.Data;
+
+@Data
 public class Shuttle extends Spaceship{
 
+    private int push;
+    private int shipWeight;
+    private int loadWeight;
+
+    public Shuttle(String name, String maker, int launchYear, String actualState, int push, int shipWeight, int loadWeight) {
+        super(name, maker, launchYear, actualState);
+        this.push = push;
+        this.shipWeight = shipWeight;
+        this.loadWeight = loadWeight;
+    }
+
+    @Override
+    public void purpose() {
+
+    }
+
+    @Override
+    public void fuelType() {
+
+    }
 }
--- src/classification/Spaceship.java
+++ src/classification/Spaceship.java
@@ -1,4 +1,27 @@
 package classification;
 
-public class Spaceship {
+
+import lombok.Data;
+
+@Data
+public abstract class Spaceship {
+
+    private String name;
+    private String maker;
+    private int launchYear;
+    private String actualState;
+
+    public Spaceship() {
+    }
+
+    public Spaceship(String name, String maker, int launchYear, String actualState) {
+        this.name = name;
+        this.maker = maker;
+        this.launchYear = launchYear;
+        this.actualState = actualState;
+    }
+
+    public abstract void purpose();
+
+    public abstract void fuelType();
 }
