93412fb63183b96fcc6d9a1af79d62197334e723
Daniel Zárate <d.ansizgz@gmail.com>
2022-07-07 22:36:20 -0500

    Construyendo las clases

---
+++ src/classification/Manned.java
@@ -0,0 +1,17 @@
+package classification;
+
+public class Manned extends Spaceship{
+
+    public Manned(){
+        super();
+    }
+    @Override
+    public void purpose() {
+
+    }
+
+    @Override
+    public void fuelType() {
+
+    }
+}
---
+++ src/classification/Unmanned.java
@@ -0,0 +1,18 @@
+package classification;
+
+public class Unmanned extends Spaceship {
+
+    public Unmanned(){
+        super();
+    }
+
+    @Override
+    public void purpose() {
+
+    }
+
+    @Override
+    public void fuelType() {
+
+    }
+}
